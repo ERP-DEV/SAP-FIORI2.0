9d5ad7b26291127d74d06ee8cf0389c626ace447
Ning-Hung <45474740+Ning-Hung@users.noreply.github.com>
2018-11-30 10:16:18 +0800

    Add files via upload

---
+++ ZMyFioriUI5/WebContent/util/connectivity.js
@@ -0,0 +1,16 @@
+//var hostPort = "44300";
+
+// this setting is required to test app with odataService in Eclipse environment
+// hostPort	 = "proxy/http/<host>:<port>";
+
+//Odata Service Path
+//var oDataPath = "/sap/opu/odata/sap/ZGW_NING_SO_SRV/";
+
+// Odata Service Url
+//var serviceUrl= hostPort + oDataPath;
+
+//OData Service URL: when testing APP in Eclipse local environment
+var serviceUrl = "proxy/http/fiori:8001/sap/opu/odata/sap/ZE2E100_NING/";
+
+//OData Service URL: when testing app in SAP FIORI LAUNCHPAD
+//var serviceUrl = "/sap/opu/odata/sap/ZGW_NING_SO_SRV/";
